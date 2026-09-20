f3e5251c8c2bd096fe46614f12724474467274e9
Kouta-Nishikawa <koutastudy2001@gmail.com>
2026-09-20 23:16:31 +0900

    スペルミスの修正./pages/tasks/taskLinkPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/home";
-import TaskLinkPage from "./pages/task/taskLinkPage";
+import TaskLinkPage from "./pages/tasks/taskLinkPage";
 
 function App () {
   return (
